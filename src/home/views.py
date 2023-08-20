da0e91b27ea7210d8637dcfd4c946dd6bdf333aa
Yevgeniy Kap <kapustaevg@gmail.com>
2023-08-20 07:35:01 +0300

    change navbar

--- src/home/views.py
+++ src/home/views.py
@@ -10,5 +10,5 @@ class HomeListView(ListView):
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['title'] = 'Главная'
+        context['title'] = 'Статьи'
         return context
