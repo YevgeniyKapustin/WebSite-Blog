e19f99d3e8503413fc6a2b2a6b62fc712f3b7c9b
Yevgeniy Kap <kapustaevg@gmail.com>
2023-08-22 11:15:03 +0300

    delete mailing

--- src/mailing/models.py
+++ src/mailing/models.py
@@ -2,7 +2,8 @@ from django.db import models
 
 
 class Contact(models.Model):
-    email = models.EmailField()
+    email = models.EmailField(unique=True)
+    is_active = models.BooleanField(default=False)
     date = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
